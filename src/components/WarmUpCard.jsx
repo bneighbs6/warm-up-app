acb42d656a1d745f6fede7f72af62e98cbfb9bc4
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-03-03 21:34:16 -0700

    Styled buttons and card titles

--- src/components/WarmUpCard.jsx
+++ src/components/WarmUpCard.jsx
@@ -5,14 +5,22 @@ import { Card } from "react-bootstrap";
 // category and exercises are props that are being passed to WarmUpGenerator
 function WarmUpCard({ category, exercises }) {
     return (
+      <div className="warmUpCard">
         <Card>
-            <Card.Title>{category.charAt(0).toUpperCase() + category.slice(1)}</Card.Title>
+          <div className="warmUpCardTitle">
+            <Card.Title>
+              {category.charAt(0).toUpperCase() + category.slice(1)}
+            </Card.Title>
+          </div>
+          <div className="warmUpCardText">
             <Card.Text>
-                    {exercises.map((exercise, index) => (
-                        <li key={index}>{exercise.exercise}</li>
-                    ))}
+              {exercises.map((exercise, index) => (
+                <li key={index}>{exercise.exercise}</li>
+              ))}
             </Card.Text>
+          </div>
         </Card>
+      </div>
     );
 }
 
--- src/components/WarmUpGenerator.jsx
+++ src/components/WarmUpGenerator.jsx
@@ -20,9 +20,9 @@ function WarmUpGenerator() {
     return (
         <div>
             <h2>Warm Up Generator</h2>
-            <button onClick={handleThreeRandomize}>Randomize a 3x3 Warm Up</button>
-            <button onClick={handleFourRandomize}>Randomize a 3x4 Warm Up</button>
-            <button onClick={handleFiveRandomize}>Randomize a 3x5 Warm Up</button>
+            <button className="warmUpButton" onClick={handleThreeRandomize}>Randomize a 3x3 Warm Up</button>
+            <button className="warmUpButton" onClick={handleFourRandomize}>Randomize a 3x4 Warm Up</button>
+            <button className="warmUpButton" onClick={handleFiveRandomize}>Randomize a 3x5 Warm Up</button>
             {exercises ? ( // Check if exercises is not null
                 Object.entries(exercises).map(([category, exerciseList]) => (
                     <div key={category}>
