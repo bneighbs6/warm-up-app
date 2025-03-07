df590235cef59d45e2497f1b06107d539fa0da15
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-03-07 13:56:10 -0700

    Placed warm up generator in a modal and designed

--- src/components/WarmUpGenerator.jsx
+++ src/components/WarmUpGenerator.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { generateThreeByThreeWarmUp, generateThreeByFourWarmUp, generateThreeByFiveWarmUp } from "../randomGenerator";
 import WarmUpCard from "./WarmUpCard";
+import { Card, Modal } from "react-bootstrap";
 
 function WarmUpGenerator() {
     const [exercises, setExercises] = useState(null); // Initialize to null
@@ -32,39 +33,52 @@ function WarmUpGenerator() {
     };
 
     return (
-        <div>
+      <div>
+        <Modal.Dialog className="warmUpGenerator">
+          <div>
             <h2>Warm Up Generator</h2>
-            <button 
-                className="warmUpButton" 
-                onClick={handleThreeRandomize} 
-                style={{ fontWeight: boldButton === 'three' ? 'bold' : 'normal', border: boldButton === 'three' ? '5px solid black' : 'none' }}
+            <button
+              className="warmUpButton"
+              onClick={handleThreeRandomize}
+              style={{
+                fontWeight: boldButton === "three" ? "bold" : "normal",
+                border: boldButton === "three" ? "5px solid black" : "none",
+              }}
             >
-                Randomize a 3x3 Warm Up
+              Randomize a 3x3 Warm Up
             </button>
-            <button 
-                className="warmUpButton" 
-                onClick={handleFourRandomize} 
-                style={{ fontWeight: boldButton === 'four' ? 'bold' : 'normal', border: boldButton === 'four' ? '5px solid black' : 'none' }}
+            <button
+              className="warmUpButton"
+              onClick={handleFourRandomize}
+              style={{
+                fontWeight: boldButton === "four" ? "bold" : "normal",
+                border: boldButton === "four" ? "5px solid black" : "none",
+              }}
             >
-                Randomize a 3x4 Warm Up
+              Randomize a 3x4 Warm Up
             </button>
-            <button 
-                className="warmUpButton" 
-                onClick={handleFiveRandomize} 
-                style={{ fontWeight: boldButton === 'five' ? 'bold' : 'normal', border: boldButton === 'five' ? '5px solid black' : 'none' }}
+            <button
+              className="warmUpButton"
+              onClick={handleFiveRandomize}
+              style={{
+                fontWeight: boldButton === "five" ? "bold" : "normal",
+                border: boldButton === "five" ? "5px solid black" : "none",
+              }}
             >
-                Randomize a 3x5 Warm Up
+              Randomize a 3x5 Warm Up
             </button>
             {exercises ? ( // Check if exercises is not null
-                Object.entries(exercises).map(([category, exerciseList]) => (
-                    <div key={category}>
-                        <WarmUpCard category={category} exercises={exerciseList} />
-                    </div>
-                ))
+              Object.entries(exercises).map(([category, exerciseList]) => (
+                <div key={category}>
+                  <WarmUpCard category={category} exercises={exerciseList} />
+                </div>
+              ))
             ) : (
-                <p>Welcome to THE Warm Up App. Click the button to randomize!</p> // Message when exercises are null
+              <p>Welcome to THE Warm Up App. Click a button to randomize!</p> // Message when exercises are null
             )}
-        </div>
+          </div>
+        </Modal.Dialog>
+      </div>
     );
 }
 
